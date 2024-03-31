d271be699147b7701031fdf0083f464ab8f4d004
Oğuz <141454604+OguzEmir177@users.noreply.github.com>
2024-03-31 20:27:04 +0300

    Create dc.py

---
+++ dc.py
@@ -0,0 +1,26 @@
+import discord
+from bilgiler import token
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$Hello'):
+        await message.channel.send("Merhaba!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("See you...")
+    else:
+        await message.channel.send(message.content)
+
+client.run(token)
