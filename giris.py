a40489bd6bb28c7b3e31371b8395d3e0621fa21e
Oğuz <141454604+OguzEmir177@users.noreply.github.com>
2024-04-25 19:07:20 +0300

    Update giris.py

--- giris.py
+++ giris.py
@@ -15,6 +15,12 @@ async def on_ready():
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.text_channels, name="hoşgeldin-reis")
     if channel:
-        await channel.send(f'Hoşgeldin, {member.mention}!')
+        await channel.send(f'Hoşgeldin_Reis, {member.mention}!')
+
+@client.event
+async def on_member_remove(member):
+    channel = discord.utils.get(member.guild.text_channels, name="gülegüle-reis")
+    if channel:
+        await channel.send(f'Güle güle Reis, {member.display_name}!')
 
 client.run(token)
